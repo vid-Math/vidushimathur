c3acd3a0eff4c5afe37ca3e9a668359e3fa7a204
Vidushi <107294109+vid-Math@users.noreply.github.com>
2023-12-22 20:36:16 +0000

    blog addition

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '8ea09c156ead72a9d83d0e7d5cd6229307229e9b',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8ea09c156ead72a9d83d0e7d5cd6229307229e9b',
+    ),
+  ),
+);
